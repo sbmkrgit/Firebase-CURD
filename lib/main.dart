846bcbdd011a3177fb6156e71ea76cca62ecb7de
Shubham <babukumar9931@gmail.com>
2020-10-16 22:53:26 +0530

    Applied Theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'HomePage.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -8,6 +10,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
   }
 }
