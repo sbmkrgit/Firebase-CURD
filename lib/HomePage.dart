ee61d89014cfcfc25b7caff7beb867f50abbc137
Shubham <babukumar9931@gmail.com>
2020-11-07 00:57:02 +0530

    Modified Home Page

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -6,10 +7,44 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  void showdialog() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Add ToDo"),
+          content: Form(
+            child: TextFormField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Task",
+              ),
+            ),
+          ),
+          actions: <Widget>[
+            RaisedButton(
+              onPressed: () {},
+              child: Text("Add"),
+            )
+          ],
+        );
+      },
+    );
+  }
+
+  final db = FirebaseFirestore.instance;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(),
-    );
+        child: Scaffold(
+      appBar: AppBar(
+        title: Text('Notes'),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: showdialog,
+        child: Icon(Icons.add),
+      ),
+    ));
   }
 }
